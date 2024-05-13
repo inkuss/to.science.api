44164d7fa0cd03a77a223f7d81e6654056641b83
Ingolf Kuss <kuss@hbz-nrw.de>
2024-05-13 14:29:04 +0200

    Erweitere Deploy-Skript um remote repository - Parameter

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,12 +9,18 @@ export TERM=xterm-color
 deployingApp="to.science.api"
 branch=$(git status | grep branch | cut -d ' ' -f3)
 echo "git currently on branch: "$branch
+remote=origin
 if [ ! -z "$1" ]; then
     branch="$1"
 fi
+if [ ! -z "$2" ]; then
+    branch="$2"
+    remote=$1
+fi
+
 
 cd /opt/toscience/git/$deployingApp
-git pull origin $branch
+git pull $remote $branch
 /opt/toscience/activator/activator -java-home /opt/jdk clean
 /opt/toscience/activator/activator -java-home /opt/jdk clean-files
 /opt/toscience/activator/activator -java-home /opt/jdk dist
